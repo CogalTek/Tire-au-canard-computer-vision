4f754a9a48e521b829a44cec5f9db44c69950e7e
azeuio <nicolas.mesrine@epitech.eu>
2026-02-18 16:33:51 +0100

    feat: add duck sprites and update target drawing logic

--- src/components/target.py
+++ src/components/target.py
@@ -19,16 +19,39 @@ class Target(ATarget):
         angle = random.uniform(0, 2 * np.pi)
         speed = random.uniform(50, 150)  # pixels par seconde
         self._velocity = (speed * np.cos(angle), speed * np.sin(angle))
+        self.sprite_right = cv2.imread("assets/duck_right.png", cv2.IMREAD_UNCHANGED)
+        self.sprite_right = cv2.resize(
+            self.sprite_right, (self.radius * 2, self.radius * 2)
+        )
+        self.sprite_up = cv2.imread("assets/duck_up.png", cv2.IMREAD_UNCHANGED)
+        self.sprite_up = cv2.resize(self.sprite_up, (self.radius * 2, self.radius * 2))
+        self.sprite_down = cv2.imread("assets/duck_down.png", cv2.IMREAD_UNCHANGED)
+        self.sprite_down = cv2.resize(
+            self.sprite_down, (self.radius * 2, self.radius * 2)
+        )
 
     def draw(self, img: MatLike):
         """Dessine le target avec un style arrondi"""
         x1, y1 = self.x - self.radius, self.y - self.radius
         x2, y2 = self.x + self.radius, self.y + self.radius
 
-        # Fond du target
-        overlay = img.copy()
-        cv2.circle(overlay, (self.x, self.y), self.radius, (0, 0, 255), -1)
-        cv2.addWeighted(overlay, 0.7, img, 0.3, 0, img)
+        # Sprite du target
+        sprite = self.sprite_up
+        if abs(self._velocity[1]) > abs(self._velocity[0]):
+            if self._velocity[1] > 0:
+                sprite = self.sprite_down
+        else:
+            sprite = self.sprite_right
+        # flip if moving left
+        if self._velocity[0] < 0:
+            sprite = cv2.flip(sprite, 1)
+        for c in range(0, 3):
+            img[y1:y2, x1:x2, c] = sprite[:, :, c] * (sprite[:, :, 3] / 255.0) + img[
+                y1:y2, x1:x2, c
+            ] * (1.0 - sprite[:, :, 3] / 255.0)
+
+    def _to_radian(self, angle):
+        return angle * np.pi / 180
 
     def update(self, delta: float):
         """Met à jour la position du target en fonction de sa vélocité"""
